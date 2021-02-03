4f7b218c1a15193adfd75128d644e4e4d85b2de0
Nicola <nicola.porta00@gmail.com>
2021-02-03 14:49:06 +0100

    Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset("css/app.css")}}">
+    <title>La Molisana</title>
+</head>
+<body>
+    <header>
+        <img src="{{ asset("img/marchio-sito-test.png")}}" alt="Logo La Molisana">
+        <nav>
+            <ul>
+                <li class="active">
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Prodotti</a>
+                </li>
+                <li>
+                    <a href="#">News</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
